59634a38734061d31b3d5e5034614922323779b0
hansaem <hansaemj20@gmail.com>
2023-11-15 14:12:35 +0900

    Section2/API Gateway Service: spring cloud gateway - filter 적용 (2) > java code로 filter 설정

---
+++ apigateway/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.example.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**") // 요청 path
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                .addResponseHeader("first-response", "first-response-header"))
+                        .uri("http://localhost:8081") // 라우팅할 uri
+                )
+                .route(r -> r.path("/second-service/**") // 요청 path
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                        .uri("http://localhost:8082") // 라우팅할 uri
+                )
+                .build(); // 실제로 메모리에 반영
+    }
+}

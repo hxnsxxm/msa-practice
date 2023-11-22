99f33b88afe6cc259d729c8e39021703fa5f93aa
hansaem <hansaemj20@gmail.com>
2023-11-22 16:31:24 +0900

    Section5/Catalogs and Orders Microservice: Users Microservice - AuthenticationFilter 추가, UserService 수정

---
+++ ms/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.example.userservice.security;
+
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+    private Environment env;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager, UserService userService, Environment env) {
+        super.setAuthenticationManager(authenticationManager);
+        this.userService = userService;
+        this.env = env;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+            HttpServletResponse response) throws AuthenticationException {
+        try {
+            // POST 로 들어온 데이터들은 request param으로 받을 수 없기 때문에 inputstream 작업
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+            return getAuthenticationManager().authenticate(
+                    // 3 번째 인자: 어떤 권한을 가질 것인지
+                    new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    // 실제 사용자가 로그인을 성공했을 때, 어떤 처리를 해줄 것인지
+    // 토큰 생성, 토큰 유효 기간 설정, ...
+    protected void successfulAuthentication(HttpServletRequest request,
+            HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+
+    }
+}
--- ms/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ ms/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,18 +1,42 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import javax.servlet.Filter;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
 @Configuration     // 다른 빌드보다 우선순위를 높여서 빌드 가능
 @EnableWebSecurity // Web Security 용도로 등록
 public class WebSecurity {
 
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;  // application.yml 에서 토큰 유효 기간 설정 등의 정보 가져옴
+
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder,
+            Environment env) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManager authenticationManager = getAuthenticationManager(http);
+
         http.csrf().disable()
             .headers(authorize -> authorize
                     // 이 설정은 웹 페이지가 다른 사이트에 삽입되는 것을 방지하기 위해 X-Frame-Options 헤더를 비활성화한다.
@@ -21,8 +45,36 @@ public class WebSecurity {
                     .frameOptions().disable()
             )
             .authorizeHttpRequests(authorize -> authorize
-                    .antMatchers("/users/**").permitAll()
+                    //.antMatchers("/users/**").permitAll()
+                    .antMatchers("/**").access(hasIpAddress("192.168.0.36")) // IP 변경
+                    .and()
+                    .authenticationManager(authenticationManager)
+                    .addFilter(getAuthenticationFilter(authenticationManager))
             );
         return http.build();
     }
+
+    private static AuthorizationManager<RequestAuthorizationContext> hasIpAddress(String ipAddress) {
+        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(ipAddress);
+        return (authentication, context) -> {
+            HttpServletRequest request = context.getRequest();
+            return new AuthorizationDecision(ipAddressMatcher.matches(request));
+        };
+    }
+
+    // select pwd from users where email=?     -> 역할 -> userDetailsService() 에 인자로 전달
+    // db_pwd(encryped) == input_pwd(encryped) -> 역할 -> passwordEncoder() 에 인자로 전달
+    private AuthenticationManager getAuthenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject((AuthenticationManagerBuilder.class));
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder((bCryptPasswordEncoder));
+
+        return authenticationManagerBuilder.build();
+    }
+
+    // AuthenticationFilter가 Filter를 상속받아서 반환값으로 사용할 수 있음
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new AuthenticationFilter(authenticationManager, userService, env);
+    }
+
+
 }

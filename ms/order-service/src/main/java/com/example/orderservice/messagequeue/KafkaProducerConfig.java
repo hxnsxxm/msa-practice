1ff264bd69c9d17063491cbbbbf6f7069103b80f
hansaem <hansaemj20@gmail.com>
2023-12-05 17:24:25 +0900

    Section12/데이터 동기화를 위한 Apache Kafka의 활용 2: Orders Microservice 수정, mysql

--- ms/order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
+++ ms/order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -24,8 +25,8 @@ public class KafkaProducerConfig {
         Map<String, Object> properties = new HashMap<>();
 
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(properties);
     }
